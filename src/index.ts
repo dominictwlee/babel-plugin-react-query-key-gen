232fa8551dd82f2cb33f9d9bbbc07b8ea8b62be8
Dominic Lee <dominictwlee@gmail.com>
2021-02-20 19:25:16 +0000

    feat: handle queryObject transform

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import * as t from "@babel/types";
-import { Visitor, NodePath } from "@babel/traverse";
+import * as t from '@babel/types';
+import { Visitor, NodePath } from '@babel/traverse';
 
 interface PluginOptions {
   opts?: {
@@ -16,24 +16,23 @@ interface Babel {
 }
 
 interface ParamsStringOnlyQueryKeySignature extends t.CallExpression {
-  arguments: [t.StringLiteral, ...t.CallExpression["arguments"]];
+  arguments: [t.StringLiteral, ...t.CallExpression['arguments']];
 }
 
 interface ParamsArrayKeySignature extends t.CallExpression {
-  arguments: [t.ArrayExpression, ...t.CallExpression["arguments"]];
+  arguments: [t.ArrayExpression, ...t.CallExpression['arguments']];
 }
 
 interface QueryObjectSignature extends t.CallExpression {
-  arguments: [t.ObjectExpression, ...t.CallExpression["arguments"]];
+  arguments: [t.ObjectExpression, ...t.CallExpression['arguments']];
 }
 
 export default function plugin(
   babel: Babel
 ): { name: string; visitor: Visitor<PluginOptions> } {
   const { types: t } = babel;
-
   return {
-    name: "ast-transform", // not required
+    name: 'babel-plugin-react-query-string-key-gen', // not required
     visitor: {
       CallExpression(path) {
         const { node } = path;
@@ -51,7 +50,6 @@ export default function plugin(
           const [arrayKey, queryFn] = node.arguments;
           let stringKeyLiteral: t.StringLiteral | null = null;
 
-          // infer string key from function name and add to existing params key
           if (t.isIdentifier(queryFn)) {
             stringKeyLiteral = t.stringLiteral(queryFn.name);
           } else if (t.isFunction(queryFn)) {
@@ -61,47 +59,73 @@ export default function plugin(
           if (stringKeyLiteral) {
             arrayKey.elements.unshift(stringKeyLiteral);
           }
+        } else if (hasQueryObject(node)) {
+          const queryObjExpression = node.arguments[0];
+          const queryKeyProperty = queryObjExpression.properties.find(
+            (p) =>
+              t.isObjectProperty(p) &&
+              t.isIdentifier(p.key) &&
+              p.key.name === 'queryKey'
+          ) as t.ObjectProperty;
+
+          if (t.isStringLiteral(queryKeyProperty?.value)) {
+            return;
+          }
+
+          const queryFnMember = queryObjExpression.properties.find(
+            (p) =>
+              t.isObjectMember(p) &&
+              t.isIdentifier(p.key) &&
+              p.key.name === 'queryFn'
+          ) as t.ObjectMember;
+
+          if (!queryFnMember) {
+            return;
+          }
+
+          const queryFnName = t.isObjectMethod(queryFnMember)
+            ? extractQueryFnNameFromBody(queryFnMember)
+            : t.isFunction(queryFnMember.value)
+            ? extractQueryFnNameFromBody(queryFnMember.value)
+            : null;
+
+          if (!queryFnName) {
+            return;
+          }
+
+          if (!queryKeyProperty) {
+            queryObjExpression.properties.push(
+              t.objectProperty(t.identifier('queryKey'), queryFnName)
+            );
+          } else {
+            if (
+              t.isArrayExpression(queryKeyProperty.value) &&
+              !t.isStringLiteral(queryKeyProperty.value[0])
+            ) {
+              queryKeyProperty.value.elements.unshift(queryFnName);
+            }
+          }
         }
-
-        // if (hasQueryObjectArg(node.arguments)) {
-        //   const queryObjExpression = node.arguments[0] as t.ObjectExpression;
-        //   const queryKeyObjProperty = queryObjExpression.properties.find(
-        //     (p) =>
-        //       t.isObjectProperty(p) &&
-        //       t.isIdentifier(p.key) &&
-        //       p.key.name === "queryKey"
-        //   ) as t.ObjectProperty;
-
-        //   if (!queryKeyObjProperty) {
-        //     const newQueryKeyProperty = t.objectProperty(
-        //       t.identifier("queryKey"),
-        //       t.stringLiteral("somethingNew")
-        //     );
-        //   }
-        // } else {
-        //   // using query parameters call sig [queryKey, queryFn, config]
-        // }
       },
     },
   };
 }
 
 function extractQueryFnNameFromBody(queryFn: t.Function) {
-  if (t.isBlockStatement(queryFn.body)) {
-    const returnStatement = queryFn.body.body.find((statement) =>
-      t.isReturnStatement(statement)
-    ) as t.ReturnStatement | undefined;
-
-    if (
-      returnStatement &&
-      t.isCallExpression(returnStatement.argument) &&
-      t.isIdentifier(returnStatement.argument.callee)
-    ) {
-      return t.stringLiteral(returnStatement.argument.callee.name);
-    }
-  } else if (t.isCallExpression(queryFn.body)) {
+  if (t.isCallExpression(queryFn.body)) {
     if (t.isIdentifier(queryFn.body.callee)) {
       return t.stringLiteral(queryFn.body.callee.name);
+    }
+  } else if (t.isBlockStatement(queryFn.body)) {
+    let queryFnName = '';
+    t.traverseFast(queryFn.body, (node) => {
+      if (t.isCallExpression(node) && t.isIdentifier(node.callee)) {
+        queryFnName = node.callee.name;
+      }
+    });
+
+    if (queryFnName) {
+      return t.stringLiteral(queryFnName);
     }
   }
 
@@ -111,7 +135,7 @@ function extractQueryFnNameFromBody(queryFn: t.Function) {
 function isUseQueryCall(
   callee: t.Expression | t.V8IntrinsicIdentifier | t.Identifier
 ): callee is t.Identifier {
-  return t.isIdentifier(callee) && callee.name === "useQuery";
+  return t.isIdentifier(callee) && callee.name === 'useQuery';
 }
 
 function hasStringOnlyQueryKeyParam(
@@ -130,9 +154,6 @@ function hasStringKeyParam(node: ParamsArrayKeySignature) {
   return t.isStringLiteral(node.arguments[0].elements[0]);
 }
 
-// function hasQueryObject(node: t.CallExpression) {
-//   return (
-//     callExpressionArgs.length === 1 &&
-//     callExpressionArgs[0].type === "ObjectExpression"
-//   );
-// }
+function hasQueryObject(node: t.CallExpression): node is QueryObjectSignature {
+  return t.isObjectExpression(node.arguments[0]);
+}
